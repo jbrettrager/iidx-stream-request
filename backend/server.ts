b53c9da65c290f0c06a5a070bb7f6f3aaab912c0
Matt Brettrager <jbrettrager94@gmail.com>
2024-03-04 18:19:20 +0900

    test deploy, more small fixes

--- backend/server.ts
+++ backend/server.ts
@@ -4,7 +4,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const mongoose = require("mongoose");
-//import {uriSecret} from "./config.ts"
+import {uriSecret} from "./config.ts"
 import { Room } from "./Room";
 import {
   Song,
